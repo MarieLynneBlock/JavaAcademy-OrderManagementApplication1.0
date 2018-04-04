ebf34eb2c06db2f07d5052249cc4127e696b2ced
Marie-Lynne Block <marielynne.block@gmail.com>
2018-04-04 02:29:43 +0200

    item RestPoints - Story 2 done

--- api/src/main/java/cm/marielynneblock/api/items/ItemController.java
+++ api/src/main/java/cm/marielynneblock/api/items/ItemController.java
@@ -1,4 +1,32 @@
 package cm.marielynneblock.api.items;
 
+import cm.marielynneblock.domain.items.Item;
+import cm.marielynneblock.service.items.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(path = {"/item"})
 public class ItemController {
+    private ItemService itemService;
+    private ItemMapper itemMapper;
+
+    @Inject
+    public ItemController(ItemService itemService, ItemMapper itemMapper) {
+        this.itemService = itemService;
+        this.itemMapper = itemMapper;
+    }
+
+    @PostMapping(produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDto addNewCustomer(@RequestBody ItemDto itemDto) {
+        return itemMapper.toDto(
+                itemService.addNewItem(
+                        itemMapper.toDomain(itemDto)));
+    }
+
 }
