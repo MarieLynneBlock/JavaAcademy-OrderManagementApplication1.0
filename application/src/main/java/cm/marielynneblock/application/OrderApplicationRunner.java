f6c0f53d9613bd50d86dbb072191a0f087d68436
Marie-Lynne Block <marielynne.block@gmail.com>
2018-04-10 08:48:01 +0200

    Fix - thanks Maarten

--- application/src/main/java/cm/marielynneblock/application/OrderApplicationRunner.java
+++ application/src/main/java/cm/marielynneblock/application/OrderApplicationRunner.java
@@ -3,7 +3,7 @@ package cm.marielynneblock.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"be.marielynneblock"})
+@SpringBootApplication(scanBasePackages = {"cm.marielynneblock"})
 public class OrderApplicationRunner {
 
     public static void main(String[] args) {
